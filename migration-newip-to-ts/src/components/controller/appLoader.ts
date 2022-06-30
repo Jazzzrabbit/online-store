6b622dc3f94311e958254a1be99bee11a8841d5b
jazzzrabbit <jazzzrabbit@gmail.com>
2022-06-30 14:55:46 +0300

    refactor: ts-ed appLoader

---
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -0,0 +1,11 @@
+import Loader from './loader';
+
+class AppLoader extends Loader {
+  constructor() {
+    super('https://newsapi.org/v2/', {
+      apiKey: '8dba3181037d4e74a733930ddcbadd4b',
+    });
+  }
+}
+
+export default AppLoader;
