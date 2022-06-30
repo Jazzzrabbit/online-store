ca405e78172045709d295527cf3f48704d986038
jazzzrabbit <jazzzrabbit@gmail.com>
2022-06-30 17:55:59 +0300

    refactor: ts-ed sources

---
+++ migration-newip-to-ts/src/components/view/sources/sources.ts
@@ -0,0 +1,23 @@
+import './sources.css';
+import { TSourceValues } from '../../../interface/interface';
+
+class Sources {
+  public draw(data: TSourceValues[]): void {
+    const fragment: DocumentFragment = document.createDocumentFragment();
+    const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement | null;
+    if (sourceItemTemp === null) throw new Error('Serious error. Deal with it.');
+    
+    data.forEach((item: TSourceValues) => {
+      const sourceClone = sourceItemTemp.content.cloneNode(true) as HTMLElement;
+
+      (sourceClone.querySelector('.source__item-name') as HTMLElement).textContent = item.name;
+      (sourceClone.querySelector('.source__item') as HTMLElement).setAttribute('data-source-id', item.id);
+
+      fragment.append(sourceClone);
+    });
+
+    (document.querySelector('.sources') as HTMLElement).append(fragment);
+  }
+}
+
+export default Sources;
