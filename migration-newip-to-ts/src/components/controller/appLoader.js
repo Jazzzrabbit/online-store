bcbdd5b378c1f201d7cd2541432aad3d4ebc2872
jazzzrabbit <jazzzrabbit@gmail.com>
2022-06-30 10:42:33 +0300

    feat: add api key and npm packages

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '8dba3181037d4e74a733930ddcbadd4b',
         });
     }
 }
