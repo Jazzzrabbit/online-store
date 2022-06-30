f7527d2d0911abf8f8c46f649370334d635f846c
jazzzrabbit <jazzzrabbit@gmail.com>
2022-06-30 15:02:03 +0300

    refactor: ts-ed controller

---
+++ migration-newip-to-ts/src/components/controller/controller.ts
@@ -0,0 +1,39 @@
+import AppLoader from './appLoader';
+
+class AppController extends AppLoader {
+  public getSources(callback: () => void): void {
+    super.getResp(
+      {
+        endpoint: 'sources',
+      },
+      callback,
+    );
+  }
+
+  public getNews(e: Event, callback: () => void): void {
+    let target = e.target as Element;
+    const newsContainer = e.currentTarget as Element;
+
+    while (target !== newsContainer) {
+      if (target.classList.contains('source__item')) {
+        const sourceId: null | string = target.getAttribute('data-source-id');
+        if (newsContainer.getAttribute('data-source') !== sourceId) {
+          newsContainer.setAttribute('data-source', sourceId as string);
+          super.getResp(
+            {
+              endpoint: 'everything',
+              options: {
+                sources: sourceId,
+              },
+            },
+            callback
+          );
+        }
+        return;
+      }
+      target = target.parentNode as Element;
+    }
+  }
+}
+
+export default AppController;
