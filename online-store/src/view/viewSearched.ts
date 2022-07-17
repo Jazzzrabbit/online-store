e75816dbb3b6d8ec54537667918d30cb6e44e52f
jazzzrabbit <jazzzrabbit@gmail.com>
2022-07-17 11:17:53 +0300

    refactor: add missing type, change innerHTML to textContent for text in viewSearched func

--- online-store/src/view/viewSearched.ts
+++ online-store/src/view/viewSearched.ts
@@ -1,19 +1,22 @@
 export default function viewSearched(): void {
   const search = document.querySelector('.header__search') as HTMLInputElement;
-  const cards = document.querySelectorAll('.card');
+  const cards: NodeListOf<Element> = document.querySelectorAll('.card');
   
   search.addEventListener('input', e => {
-    const inputValue = (e.target as HTMLInputElement).value.toLocaleLowerCase();
+    const inputValue: string = (e.target as HTMLInputElement).value.toLocaleLowerCase();
     
     cards.forEach(card => {
-      if (!card.querySelector('.list__item')?.innerHTML.split(': ')[1].toLocaleLowerCase().includes(inputValue)) {
+      if (!(card.querySelector('.list__item')?.textContent as string)
+        .split(': ')[1].toLocaleLowerCase().includes(inputValue)) {
         card.classList.add('transformed');
         setTimeout(() => {
           card.classList.add('hidden');
         }, 400);
       } else {
         card.classList.remove('transformed');
-        card.classList.remove('hidden');
+        setTimeout(() => {
+          card.classList.remove('hidden');
+        }, 400);
       }
     });
   });
