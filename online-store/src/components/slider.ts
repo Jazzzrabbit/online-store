39e473c67c8b9c90335184991f97c9c45feae16a
jazzzrabbit <jazzzrabbit@gmail.com>
2022-07-17 10:01:30 +0300

    refactor: change slider func using array destructuring

--- online-store/src/components/slider.ts
+++ online-store/src/components/slider.ts
@@ -17,10 +17,10 @@ export default function createSlider() {
       'max': 8,
     },
     format: {
-      to: function (value) {
+      to: function (value: number): number {
         return value;
       },
-      from: function (value) {
+      from: function (value: string): number {
         return +value;
       },
     },
@@ -35,10 +35,10 @@ export default function createSlider() {
       'max': 2022,
     },
     format: {
-      to: function (value) {
+      to: function (value: number): number {
         return value;
       },
-      from: function (value) {
+      from: function (value: string): number {
         return +value;
       },
     },
@@ -46,13 +46,15 @@ export default function createSlider() {
 
   amountSlider.noUiSlider?.on('change', values => {
     const cards: ICard[] = cardsOnScreen();
-    const slidedArr: Array<ICard> = cards.filter(card => card.amount >= values[0] && card.amount <= values[1]);
+    const [minValue, maxValue] = values;
+    const slidedArr: Array<ICard> = cards.filter(card => card.amount >= minValue && card.amount <= maxValue);
     viewCards(slidedArr);
   });
 
   yearSlider.noUiSlider?.on('change', values => {
     const cards: ICard[] = cardsOnScreen();
-    const slidedArr: Array<ICard> = cards.filter(card => card.year >= values[0] && card.year <= values[1]);
+    const [minValue, maxValue] = values;
+    const slidedArr: Array<ICard> = cards.filter(card => card.year >= minValue && card.year <= maxValue);
     viewCards(slidedArr);
   });
 }
