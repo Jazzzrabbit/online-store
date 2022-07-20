1ba6054b21c8a4f383b6ae622c74a9247c811faf
jazzzrabbit <jazzzrabbit@gmail.com>
2022-07-20 22:17:15 +0300

    refactor: change innerHtml to textContent for text, add missing types, change for-of cycle appendChild to append(...args) in sortFunctions

--- online-store/src/components/sortFunctions.ts
+++ online-store/src/components/sortFunctions.ts
@@ -2,62 +2,50 @@ export function sortByNameHigh(): void {
   const cards: Element[] = [...document.querySelectorAll('.card')];
   const content = document.getElementById('content') as HTMLElement;
 
-  const sorted = cards.sort((card1, card2) => {
-    return (card1.querySelector('.name')?.innerHTML as string) >
-      (card2.querySelector('.name')?.innerHTML as string) ? 1 : -1;
+  const sorted: Element[] = cards.sort((card1, card2) => {
+    return (card1.querySelector('.name')?.textContent as string) >
+      (card2.querySelector('.name')?.textContent as string) ? 1 : -1;
   });
 
   content.innerHTML = '';
-
-  for (const card of sorted) {
-    content.appendChild(card);
-  }
+  content.append(...sorted);
 }
 
 export function sortByNameLow(): void {
   const cards: Element[] = [...document.querySelectorAll('.card')];
   const content = document.getElementById('content') as HTMLElement;
 
-  const sorted = cards.sort((card1, card2) => {
-    return (card1.querySelector('.name')?.innerHTML as string) <
-      (card2.querySelector('.name')?.innerHTML as string) ? 1 : -1;
+  const sorted: Element[] = cards.sort((card1, card2) => {
+    return (card1.querySelector('.name')?.textContent as string) <
+      (card2.querySelector('.name')?.textContent as string) ? 1 : -1;
   });
 
   content.innerHTML = '';
-
-  for (const card of sorted) {
-    content.appendChild(card);
-  }
+  content.append(...sorted);
 }
 
 export function sortByYearHigh(): void {
   const cards: Element[] = [...document.querySelectorAll('.card')];
   const content = document.getElementById('content') as HTMLElement;
 
-  const sorted = cards.sort((card1, card2) => {
-    return +(card2.querySelector('.year')?.innerHTML as string).split(': ')[1] -
-      +(card1.querySelector('.year')?.innerHTML as string).split(': ')[1];
+  const sorted: Element[] = cards.sort((card1, card2) => {
+    return +(card2.querySelector('.year')?.textContent as string).split(': ')[1] -
+      +(card1.querySelector('.year')?.textContent as string).split(': ')[1];
   });
 
   content.innerHTML = '';
-
-  for (const card of sorted) {
-    content.appendChild(card);
-  }
+  content.append(...sorted);
 }
 
 export function sortByYearLow(): void {
   const cards: Element[] = [...document.querySelectorAll('.card')];
   const content = document.getElementById('content') as HTMLElement;
 
-  const sorted = cards.sort((card1, card2) => {
-    return +(card1.querySelector('.year')?.innerHTML as string).split(': ')[1] -
-      +(card2.querySelector('.year')?.innerHTML as string).split(': ')[1];
+  const sorted: Element[] = cards.sort((card1, card2) => {
+    return +(card1.querySelector('.year')?.textContent as string).split(': ')[1] -
+      +(card2.querySelector('.year')?.textContent as string).split(': ')[1];
   });
 
   content.innerHTML = '';
-
-  for (const card of sorted) {
-    content.appendChild(card);
-  }
+  content.append(...sorted);
 }
